6445e74708acb090cd15cb6ff0cbed9895043a39
EliaGiolli <146369060+EliaGiolli@users.noreply.github.com>
2025-04-15 20:23:38 +0200

    Add files via upload

--- src/custom hooks/useSearchRecipies.tsx
+++ src/custom hooks/useSearchRecipies.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { RecipeSearch } from "../utils/types";
+import { RecipeDetails } from "../utils/types";
 
 export function useFetch(searchRecipies: string) {
   const [recipies, setRecipies] = useState<RecipeSearch[]>([]);
@@ -50,4 +51,53 @@ export function useFetch(searchRecipies: string) {
   }, [searchRecipies]);
 
   return { recipies, loading, error };
+}
+
+export function useFetchDetails(searchRecipies: string) {
+  const [recipiesDetail, setRecipiesDetail] = useState<RecipeDetails | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (searchRecipies.trim() === "") {
+      setRecipiesDetail(null);
+      setLoading(false);
+      return;
+    }
+
+    const controller = new AbortController();
+
+    const delayDebounce = setTimeout(() => {
+      setLoading(true);
+      setError(null);
+
+      const apiKey: string = "53e2afa895d944b4b96f71204850f1e5";
+
+      axios
+        .get(`https://api.spoonacular.com/recipes/${searchRecipies}/information`, { 
+          signal: controller.signal,
+          params: {
+            apiKey: apiKey,
+          },
+        })
+        .then((response) => {
+          setRecipiesDetail(response.data); 
+        })
+        .catch((err) => {
+          if (axios.isCancel(err)) return;
+          setError(err.message);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    }, 400);
+
+    // cleanup
+    return () => {
+      clearTimeout(delayDebounce);
+      controller.abort();
+    };
+  }, [searchRecipies]);
+
+  return { recipiesDetail, loading, error };
 }
